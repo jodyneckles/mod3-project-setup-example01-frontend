4a28e9459c7c8585d488b43d87148e2c8259df71
jodyneckles <jody@technative.tv>
2019-01-20 17:57:32 +0000

    done

---
+++ invoicer/src/index.js
@@ -0,0 +1,16 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//   const app = new App();
+//   app.attachEventListeners();
+//
+//   app.adapter.fetchNotes().then(json => {
+//     json.forEach(note => {
+//       document.querySelector('#notes-list').innerHTML += new Note(note).renderListItem();
+//     });
+//   });
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const app = new App();
+  app.attachEventListeners();
+  app.adapter.fetchNotes().then(app.createNotes);
+});
